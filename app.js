d3e15cff716580ce24e7398fc43402126ac1fa84
Marat <maratos2001@mail.ru>
2023-04-18 18:22:30 +0300

    add post method to send request to mailchimp and subscribe a user

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const bodyParser = require("body-parser")
 const express = require("express")
+const https = require("https");
+require("dotenv").config()
 const port = 3000
 const app = express()
+const apiKey = process.env.API_KEY
+const listId = process.env.LIST_ID
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static("public"))
 
@@ -11,4 +15,35 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/signup.html`)
+})
+
+app.post("/", (req, res) => {
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+    const jsonData = JSON.stringify(data)
+    const url = `https://us14.api.mailchimp.com/3.0/lists/${listId}?skip_merge_validation=true&skip_duplicate_check=true`
+    const options = {
+        method: "POST",
+        auth: `crescent17:${apiKey}`
+    }
+    const request = https.request(url, options, response => {
+        response.on("data", data => {
+            console.log(JSON.parse(data))
+        })
+    })
+    request.write(jsonData)
+    request.end()
 })
